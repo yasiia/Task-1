1536778ea0600aa3332696f68f0d4467656896aa
yasiia <malykyasya@gmail.com>
2024-08-17 20:41:55 +0300

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the year: ");
+            string userYear = Console.ReadLine();
+            int year = int.Parse(userYear);
+            if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0)
+            {
+                Console.WriteLine("Leap year");
+            }
+            else
+            {
+                Console.WriteLine("Not a leap year");
+            }
+        }
+    }
+}
